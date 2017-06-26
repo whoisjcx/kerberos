ca53aec9a18c8ba8ccb53061598b538b770aa158
chenlvhao <chenlvhao@hao>
2017-06-26 14:55:37 +0800

    TGS

---
+++ src/TGS.java
@@ -0,0 +1,94 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class TGS {
+	
+	private int port;	//¼àÌý¶Ë¿Ú
+	
+	class SendThread extends Thread{
+		private Socket socket=null;  
+		private BufferedReader reader;
+		private PrintWriter writer;
+		
+		public SendThread(Socket socket){
+			this.socket=socket;
+		}
+		
+		@Override
+		public void run(){
+			//String ip=socket.getInetAddress().getHostAddress();
+			try {
+				reader=new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				writer=new PrintWriter(socket.getOutputStream());
+				String str="";
+				String tem="";
+				
+				while((tem=reader.readLine())!=null)
+				{
+					str+=tem;
+				}
+				
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			//!!!!!!!!!!!!!!!!!!!!!
+			
+			String willsend="";
+			writer.println(willsend);
+			writer.flush();
+			
+			writer.close();
+			try {
+				reader.close();
+				socket.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	class ListenThread extends Thread{
+		
+		private ServerSocket server=null;
+		Socket socket=null;
+		@Override
+		public void run(){
+			try {
+					server=new ServerSocket(port);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+			}	
+			while(true){
+				try {
+					socket=server.accept();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				if(socket!=null)
+				{
+					new SendThread(socket).start();
+				}
+				try {
+					server.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+	
+	public void ASstart()
+	{
+		new ListenThread().start();
+	}
+}
+
