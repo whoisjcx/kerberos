8ae8dbf4159d75e83a0ed9188dd331fae71587ed
Administrator <Administrator@USER-20151226PH>
2017-06-27 10:05:34 +0800

    -

--- src/data.java
+++ src/data.java
@@ -137,7 +137,7 @@ public class data {
 	public static void main(String[] args){
 		int x=19;
 		String s=Integer.toString(x);
-		s="000"+s;
+		s="0000"+s;
 		System.out.println(new Integer(s));
     }
 	
